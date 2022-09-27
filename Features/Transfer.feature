2ab8b69f8da7d8b4559ce87c7a1ff988540be4de
OliverBerry504 <berry.m.oliver@gmail.com>
2022-09-28 10:01:48 +1300

    Add project files.

---
+++ Features/Transfer.feature
@@ -0,0 +1,92 @@
+﻿Feature: Transfers
+
+	In order to transfer money between my bank accounts
+	As a customer
+	I need the resulting balances to be correctly displayed
+
+@Transfers
+Scenario Outline: Transfer from Everyday to Investment
+	Given the Everyday account has a balance of 100
+	And the Investment account has a balance of 100
+	When the customer <boolIsStaff> a staff member
+	And the amount transfered between everyday and investment is <numAmount>
+	Then the Everyday account will have a balance of <resultFrom>
+	Then the Investment account will have a balance of <resultToo>
+
+	Examples:
+	| numAmount | resultFrom | resultToo | boolIsStaff |
+	| 50        | 50         | 150       | false     |
+	| 150       | 100        | 100       | false     |
+	| 150       | 100        | 100       | true      |
+
+Scenario: Transfer from Everyday to Omni
+	Given the Everyday account has a balance of 100
+	And the Omni account has a balance of 100
+	When the customer <boolIsStaff> a staff member
+	And the amount transfered between everyday and omni is <numAmount>
+	Then the Everyday account will have a balance of <resultFrom>
+	And the Omni account will have a balance of <resultToo>
+
+	Examples:
+	| numAmount | resultFrom | resultToo | boolIsStaff |
+	| 50        | 50         | 150       | false     |
+	| 150       | 100        | 100       | false     |
+	| 150       | 100        | 100       | true      |
+
+Scenario: Transfer from Investment to Everyday
+	Given the Investment account has a balance of 100
+	And the Everyday account has a balance of 100
+	When the customer <boolIsStaff> a staff member
+	And the amount transfered between investment and everyday is <numAmount>
+	Then the Investment account will have a balance of <resultFrom>
+	And the Everyday account will have a balance of <resultToo>
+
+	Examples:
+	| numAmount | resultFrom | resultToo | boolIsStaff |
+	| 50        | 50         | 150       | false     |
+	| 150       | 90         | 100       | false     |
+	| 150       | 95         | 100       | true      |
+
+Scenario: Transfer from Investment to Omni
+	Given the Investment account has a balance of 100
+	And the Omni account has a balance of 100
+	When the customer <boolIsStaff> a staff member
+	And the amount transfered between investment and omni is <numAmount>
+	Then the Investment account will have a balance of <resultFrom>
+	And the Omni account will have a balance of <resultToo>
+
+	Examples:
+	| numAmount | resultFrom | resultToo | boolIsStaff |
+	| 50        | 50         | 150       | false     |
+	| 150       | 90         | 100       | false     |
+	| 150       | 95         | 100       | true      |
+
+Scenario: Transfer from Omni to Everyday
+	Given the Omni account has a balance of 100
+	And the Everyday account has a balance of 100
+	When the customer <boolIsStaff> a staff member
+	And the amount transfered between omni and everyday is <numAmount>
+	Then the Omni account will have a balance of <resultFrom>
+	And the Everyday account will have a balance of <resultToo>
+
+	Examples:
+	|numAmount	|resultFrom	|resultToo	| boolIsStaff |
+	|50	        |50	        |150	    | false     |
+	|1000	    |-900	    |1100	    | false     |
+	|10000	    |90	        |100	    | false     |
+	|10000	    |95	        |100	    | true      |
+
+Scenario: Transfer from Omni to Investment
+	Given the Omni account has a balance of 100
+	And the Investment account has a balance of 100
+	When the customer <boolIsStaff> a staff member
+	And the amount transfered between omni and investment is <numAmount>
+	Then the Omni account will have a balance of <resultFrom>
+	And the Investment account will have a balance of <resultToo>
+
+	Examples:
+	|numAmount	|resultFrom	|resultToo	| boolIsStaff |
+	|50	        |50	        |150	    | false     |
+	|1000	    |-900	    |1100	    | false     |
+	|10000	    |90	        |100	    | false     |
+	|10000	    |95	        |100	    | true      |
---
+++ Steps/TransferStepDefinitions.cs
@@ -0,0 +1,141 @@
+﻿using BIT706_A3_OliverBerry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace BIT706_A3.Steps
+{
+    [Binding]
+    public sealed class StepDefinition1
+    {
+        private static readonly Customer customer = new Customer("Mike", false);
+
+        private Everyday _everyday = new Everyday(customer);
+        private Investment _investment = new Investment(customer);
+        private Omni _omni = new Omni(customer);
+        private AccController _accCtrl = new AccController();
+        private double _result;
+        Exception _error;
+
+        [Given(@"the Everyday account has a balance of (.*)")]
+        public void GivenTheEverydayAccHasABalanceOf(double amount)
+        {   
+            
+            _everyday.Balance = amount;
+        }
+
+        [Given(@"the Omni account has a balance of (.*)")]
+        public void GivenTheOmniAccountHasABalanceOf(double amount)
+        {
+            _omni.Balance = amount;
+        }
+
+        [Given(@"the Investment account has a balance of (.*)")]
+        public void GivenTheInvestmentAccountHasABalanceOf(double amount)
+        {
+            _investment.Balance = amount;
+        }
+
+        [When(@"the amount transfered between everyday and investment is (.*)")]
+        public void WhenTheAmountTransferedBetweenEverydayAndInvestmentIs(double amount)
+        {
+            try
+            {
+                _accCtrl.Transfer(_investment, _everyday, amount);
+            }
+            catch (Exception e)
+            {
+                _error = e;
+            }
+        }
+
+        [When(@"the amount transfered between everyday and omni is (.*)")]
+        public void WhenTheAmountTransferedBetweenEverydayAndOmniIs(double amount)
+        {
+            try
+            {
+                _accCtrl.Transfer(_omni, _everyday, amount);
+            }
+            catch (Exception e)
+            {
+                _error = e;
+            }
+        }
+
+        [When(@"the amount transfered between investment and everyday is (.*)")]
+        public void WhenTheAmountTransferedBetweenInvestmentAndEverydayIs(double amount)
+        {
+            try
+            {
+                _accCtrl.Transfer(_everyday, _investment, amount);
+            }
+            catch (Exception e)
+            {
+                _error = e;
+            }
+        }
+
+        [When(@"the amount transfered between investment and omni is (.*)")]
+        public void WhenTheAmountTransferedBetweenInvestmentAndOmniIs(double amount)
+        {
+            try
+            {
+                _accCtrl.Transfer(_omni, _investment, amount);
+            }
+            catch (Exception e)
+            {
+                _error = e;
+            }
+        }
+
+        [When(@"the amount transfered between omni and everyday is (.*)")]
+        public void WhenTheAmountTransferedBetweenOmniAndEverydayIs(double amount)
+        {
+            try
+            {
+                _accCtrl.Transfer(_everyday, _omni, amount);
+            }
+            catch (Exception e)
+            {
+                _error = e;
+            }
+        }
+
+        [When(@"the amount transfered between omni and investment is (.*)")]
+        public void WhenTheAmountTransferedBetweenOmniAndinvestmentIs(double amount)
+        {
+            try
+            {
+                _accCtrl.Transfer(_investment, _omni, amount);
+            }
+            catch (Exception e)
+            {
+                _error = e;
+            }
+        }
+
+        [Then(@"the Everyday account will have a balance of (.*)")]
+        public void ThenTheEverydayAccountWillHaveABalanceOf(double amount)
+        {
+            Assert.AreEqual(_everyday.Balance, amount);
+        }
+
+        [Then(@"the Omni account will have a balance of (.*)")]
+        public void ThenTheOmniAccountWillHaveABalanceOf(double amount)
+        {
+            Assert.AreEqual(_omni.Balance, amount);
+        }
+
+        [Then(@"the Investment account will have a balance of (.*)")]
+        public void ThenTheInvestmentAccountWillHaveABalanceOf(double amount)
+        {
+            Assert.AreEqual(_investment.Balance, amount);
+        }
+
+        [When(@"the customer (.*) a staff member")]
+        public void WhenTheCustomerAStaffMember(bool x)
+        {
+            customer.IsStaff = x;
+        }
+    }
+}
